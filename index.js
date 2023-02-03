7a5e35e4a2b8886fb781dc513fc4fe26b7be08cf
sunejakaran1998 <117005470+sunejakaran1998@users.noreply.github.com>
2023-02-03 12:13:14 +0530

    updated intex.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! Its new build')
+  response.send('Hello Karan!')
 })
 
 app.listen(app.get('port'), function() {
