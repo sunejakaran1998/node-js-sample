e912a29d55244398c49935cf18eb3a71a36c9585
sunejakaran1998 <117005470+sunejakaran1998@users.noreply.github.com>
2024-01-24 00:05:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello IDC Technologies!')
+  response.send('Hello Karan!')
 })
 app.get('/api', function(request, response) {
   response.send('Hello World!')
